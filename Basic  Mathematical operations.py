894fbe60a72d71a6e0d9c384d6c47bbb7322d2cc
Shiva20250 <shivanandjoishy9@gmail.com>
2025-06-15 13:22:08 +0530

    Add files via upload

---
+++ Basic  Mathematical operations.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the Second number: "))
+Addition=a+b
+subtraction=a-b
+Multiplication=a*b
+Division=a/b
+print("Addition: ", Addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
